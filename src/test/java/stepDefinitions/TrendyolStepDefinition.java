b557ce2c371692ee878bb57225240fd467fa1b61
kansuorhan <kansuorhan@gmail.com>
2022-10-09 15:35:28 +0300

    v1

---
+++ src/test/java/stepDefinitions/TrendyolStepDefinition.java
@@ -0,0 +1,47 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+public class TrendyolStepDefinition {
+    @Given("kullanici google a gider")
+    public void kullaniciGoogleAGider() {
+    }
+
+    @Then("kullanici google da Trendyol u aratir")
+    public void kullaniciGoogleDaTrendyolUAratir() {
+    }
+
+    @Then("kullanici Trendyol a girer")
+    public void kullaniciTrendyolAGirer() {
+    }
+
+    @Then("kullanici Trendyol da makas aratir")
+    public void kullaniciTrendyolDaMakasAratir() {
+    }
+
+    @And("kullanici toplam makas sayisini alir")
+    public void kullaniciToplamMakasSayisiniAlir() {
+    }
+
+    @Then("kullanici google da Morhipo u aratir")
+    public void kullaniciGoogleDaMorhipoUAratir() {
+    }
+
+    @Then("kullanici Morhipo ya girer")
+    public void kullaniciMorhipoYaGirer() {
+    }
+
+    @Then("kullanici Morhipo da makas aratir")
+    public void kullaniciMorhipoDaMakasAratir() {
+    }
+
+    @Given("kullanici makas sayisi fazla olan sayfayi kapatir")
+    public void kullaniciMakasSayisiFazlaOlanSayfayiKapatir() {
+    }
+
+    @Then("kullanici makas sayisi az olan sayfayi kapatir")
+    public void kullaniciMakasSayisiAzOlanSayfayiKapatir() {
+    }
+}
---
+++ src/test/resources/features/trendyol_morhipo.feature
@@ -0,0 +1,22 @@
+Feature: Trendyol Search
+
+  Scenario: US001 kullanici Trendyol sitesinde makas aratir
+
+    Given kullanici google a gider
+    Then kullanici google da Trendyol u aratir
+    Then kullanici Trendyol a girer
+    Then kullanici Trendyol da makas aratir
+    And kullanici toplam makas sayisini alir
+
+
+  Scenario: US002 kullanici Morhipo sitesinde makas aratir
+    Given kullanici google a gider
+    Then kullanici google da Morhipo u aratir
+    Then kullanici Morhipo ya girer
+    Then kullanici Morhipo da makas aratir
+    And kullanici toplam makas sayisini alir
+
+
+  Scenario: US003 kullanici makas sayilarini karsilastirir
+    Given kullanici makas sayisi fazla olan sayfayi kapatir
+    Then kullanici makas sayisi az olan sayfayi kapatir
