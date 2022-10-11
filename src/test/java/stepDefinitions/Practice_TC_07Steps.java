52223f5a3cad7e4ffdd2bdccc0a4298db6bdb328
kansuorhan <kansuorhan@gmail.com>
2022-10-11 17:52:12 +0300

    v1

---
+++ src/test/java/stepDefinitions/Practice_TC_07Steps.java
@@ -0,0 +1,27 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+public class Practice_TC_07Steps {
+    @Given("Tarayiciyi baslatarak {string} url'sine gidin")
+    public void tarayiciyiBaslatarakUrlSineGidin(String arg0) {
+    }
+
+    @Then("Ana sayfanin basariyla gorunur oldugunu dogrulayin")
+    public void anaSayfaninBasariylaGorunurOldugunuDogrulayin() {
+    }
+
+    @Then("Test Case buttonu na tiklayin")
+    public void testCaseButtonuNaTiklayin() {
+    }
+
+    @And("Kullanicinin test case sayfasina basariyla yönlendirildigini dogrulayin")
+    public void kullanicininTestCaseSayfasinaBasariylaYönlendirildiginiDogrulayin() {
+    }
+
+    @And("Ilgili sayfanin ekran goruntusunu alin")
+    public void ılgiliSayfaninEkranGoruntusunuAlin() {
+    }
+}
