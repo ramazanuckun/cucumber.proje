a5f5b1f39a986aaeccd19bc26f3f764dddc77edd
kansuorhan <kansuorhan@gmail.com>
2022-10-11 18:25:46 +0300

    v1

--- src/test/java/stepDefinitions/Practice_TC_07Steps.java
+++ src/test/java/stepDefinitions/Practice_TC_07Steps.java
@@ -4,10 +4,15 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import org.junit.Assert;
+import pages.AutomationExercisePage;
 import utilities.ConfigReader;
 import utilities.Driver;
+import utilities.ReusableMethods;
+
+import java.io.IOException;
 
 public class Practice_TC_07Steps {
+    AutomationExercisePage automationExercisePage = new AutomationExercisePage();
     @Given("Tarayiciyi baslatarak {string} url'sine gidin")
     public void tarayiciyiBaslatarakUrlSineGidin(String searchUrl) {
         Driver.getDriver().get(searchUrl);
@@ -16,19 +21,26 @@ public class Practice_TC_07Steps {
 
     @Then("Ana sayfanin basariyla gorunur oldugunu dogrulayin")
     public void anaSayfaninBasariylaGorunurOldugunuDogrulayin() {
-        String homeUrl = "https://automationexercise.com/";
-        Assert.assertEquals(homeUrl, "http://automationexercise.com/");
+        String homeUrl ="https://automationexercise.com/";
+        Assert.assertEquals("home url eşit değil",homeUrl,Driver.getDriver().getCurrentUrl());
     }
 
     @Then("Test Case buttonu na tiklayin")
     public void testCaseButtonuNaTiklayin() {
+        automationExercisePage.testCaseLink.click();
     }
 
     @And("Kullanicinin test case sayfasina basariyla yönlendirildigini dogrulayin")
     public void kullanicininTestCaseSayfasinaBasariylaYönlendirildiginiDogrulayin() {
+        Assert.assertTrue(automationExercisePage.testCaseTitle.isDisplayed());
     }
 
     @And("Ilgili sayfanin ekran goruntusunu alin")
     public void ılgiliSayfaninEkranGoruntusunuAlin() {
+        try {
+            ReusableMethods.getScreenshot("Automation Page");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
