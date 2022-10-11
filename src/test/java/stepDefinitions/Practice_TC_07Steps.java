36955168e1a47c1f3293302da6a30f1327b85d92
kansuorhan <kansuorhan@gmail.com>
2022-10-11 18:02:47 +0300

    v1

--- src/test/java/stepDefinitions/Practice_TC_07Steps.java
+++ src/test/java/stepDefinitions/Practice_TC_07Steps.java
@@ -3,14 +3,21 @@ package stepDefinitions;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import org.junit.Assert;
+import utilities.ConfigReader;
+import utilities.Driver;
 
 public class Practice_TC_07Steps {
     @Given("Tarayiciyi baslatarak {string} url'sine gidin")
-    public void tarayiciyiBaslatarakUrlSineGidin(String arg0) {
+    public void tarayiciyiBaslatarakUrlSineGidin(String searchUrl) {
+        Driver.getDriver().get(searchUrl);
+
     }
 
     @Then("Ana sayfanin basariyla gorunur oldugunu dogrulayin")
     public void anaSayfaninBasariylaGorunurOldugunuDogrulayin() {
+        String homeUrl = "https://automationexercise.com/";
+        Assert.assertEquals(homeUrl, "http://automationexercise.com/");
     }
 
     @Then("Test Case buttonu na tiklayin")
