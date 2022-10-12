cc46c75e48038d02ed44c3771c971fa0bee52598
kansuorhan <kansuorhan@gmail.com>
2022-10-12 22:46:49 +0300

    v1

---
+++ src/test/java/pages/HerOkuAppPage.java
@@ -0,0 +1,20 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+public class HerOkuAppPage {
+
+    public HerOkuAppPage(){
+
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    @FindBy(xpath = "//*[@onclick='addElement()']")
+    public WebElement addElement;
+
+    @FindBy(xpath = "//*[text()='Delete']")
+    public WebElement deleteElement;
+}
