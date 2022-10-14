2c797a6dc59a98ef3ab616f4d2e68d4ec3610690
kansuorhan <kansuorhan@gmail.com>
2022-10-14 22:46:02 +0300

    v1

--- src/test/java/stepDefinitions/WebDriverStepDefinition.java
+++ src/test/java/stepDefinitions/WebDriverStepDefinition.java
@@ -2,26 +2,43 @@ package stepDefinitions;
 
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.interactions.Actions;
+import pages.WebDriverUniversityPage;
+import utilities.Driver;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class WebDriverStepDefinition {
+
+    WebDriverUniversityPage webDriverUniversityPage = new WebDriverUniversityPage();
     @Then("kullanici Login Portala kadar asagi tiklar")
     public void kullaniciLoginPortalaKadarAsagiTiklar() {
+        Actions actions = new Actions(Driver.getDriver());
+        actions.sendKeys(Keys.ARROW_DOWN).perform();
     }
 
     @And("kullanici Login Portala tiklar")
     public void kullaniciLoginPortalaTiklar() {
+        webDriverUniversityPage.login.click();
     }
 
     @And("kullanici diger windowa gecer")
     public void kullaniciDigerWindowaGecer() {
+        ArrayList<String> windows = new ArrayList<String>(Driver.getDriver().getWindowHandles());
+        Driver.getDriver().switchTo().window(windows.get(1));
     }
 
     @And("kullanici {string} kutusuna bilgileri girer")
-    public void kullaniciKutusunaBilgileriGirer(String arg0) {
+    public void kullaniciKutusunaBilgileriGirer(String username) {
+        webDriverUniversityPage.username.sendKeys(username);
+
     }
 
     @And("kullanici {string} bolumune bilgileri girer")
-    public void kullaniciBolumuneBilgileriGirer(String arg0) {
+    public void kullaniciBolumuneBilgileriGirer(String password) {
+        webDriverUniversityPage.password.sendKeys(password);
     }
 
     @And("kullanici login butonuna basar")
