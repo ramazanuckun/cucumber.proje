ef65d7e0d0478cf38d834af288b2fdeb3746046f
kansuorhan <kansuorhan@gmail.com>
2022-10-17 21:51:21 +0300

    v1

---
+++ src/test/java/stepDefinitions/AutoStepDefinition.java
@@ -0,0 +1,34 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+
+public class AutoStepDefinition {
+    @And("user sign in linkine tiklar")
+    public void userSignInLinkineTiklar() {
+    }
+
+    @And("user Create and account bölümüne email adresi girer")
+    public void userCreateAndAccountBölümüneEmailAdresiGirer() {
+    }
+
+    @And("Create an Account butonuna basar")
+    public void createAnAccountButonunaBasar() {
+    }
+
+    @And("kullanici kisisel bilgilerini ve iletisim bilgilerini girer")
+    public void kullaniciKisiselBilgileriniVeIletisimBilgileriniGirer() {
+    }
+
+    @And("kullanici Register butonuna basar")
+    public void kullaniciRegisterButonunaBasar() {
+    }
+
+    @Then("kullanicinin hesap olusturuldugunu dogrulayin")
+    public void kullanicininHesapOlusturuldugunuDogrulayin() {
+    }
+
+    @Then("hesap olustugunu dogrulayin")
+    public void hesapOlustugunuDogrulayin() {
+    }
+}
